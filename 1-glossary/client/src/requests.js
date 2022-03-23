a5aec8a38d432dc7788abb42da6ab3d89231dfe6
Matthew Pak <mattpak@gmail.com>
2022-03-23 15:49:01 -0700

    Fixed client requests.js return data parsing with dataFix fx; client fetchAll now initializing App.jsx properly (TESTED)

--- 1-glossary/client/src/requests.js
+++ 1-glossary/client/src/requests.js
@@ -1,16 +1,27 @@
 import axios from 'axios';
 
+// response data parser
+const dataFix = (wordObjArray) => {
+  const reparse = (wordObj) => {
+    return {
+      word: wordObj._id,
+      definition: wordObj.definition
+    }
+  };
+  return wordObjArray.map(wordObj => reparse(wordObj));
+}
+
 // fetch all request
 const fetchAll = (callback) => {
   axios.get('/words')
-    .then((response) => callback(response.data))
+    .then((response) => callback(dataFix(response.data)))
     .catch((err) => console.log(err))
 }
 
 // fetch search request
 const fetchSearch = (word, callback) => {
   axios.get('/words', { params: { word } })
-    .then((response) => callback(response.data))
+    .then((response) => callback(dataFix(response.data)))
     .catch((err) => console.log(err))
 }
 
