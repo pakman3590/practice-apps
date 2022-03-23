079b76c68706da2b46e4e54cb07d8c01b510b1cb
Matthew Pak <mattpak@gmail.com>
2022-03-23 10:11:55 -0700

    Set up db connection, wordSchema + model, and addWord fx (untested)

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,6 +1,32 @@
 const mongoose = require("mongoose");
 
+async function main() {
+  await mongoose.connect(process.env.MONGOURL || 'mongodb://localhost:3000/glossary' {
+
+  })
+}
+
+
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
+
+const wordSchema = new Schema({
+  word: {
+    name: String,
+    unique: true
+  },
+  definition: String
+});
+
+const Word = mongoose.model('Word', wordSchema);
+
+
+// add word fx
+const addWord = (word, definition, callback) => {
+  const newWord = new Word({ word, definition });
+  newWord.save()
+    .then(callback(null))
+    .catch(callback(err))
+}
