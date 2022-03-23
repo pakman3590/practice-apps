17c8de35c08a3004f8235f1e6e269730e4b2d652
Matthew Pak <mattpak@gmail.com>
2022-03-23 15:32:37 -0700

    Built out all client axios requests in requests.js (UNTESTED)

---
+++ 1-glossary/client/src/requests.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+// fetch all request
+const fetchAll = (callback) => {
+  axios.get('/words')
+    .then((response) => callback(response))
+    .catch((err) => console.log(err))
+}
+
+// fetch search request
+const fetchSearch = (word, callback) => {
+  axios.get('/words', { params: { word } })
+    .then((response) => callback(response))
+    .catch((err) => console.log(err))
+}
+
+// post new request
+const postNew = (wordObj, callback) => {
+  axios.post('/words', wordObj)
+    .then(() => console.log('Word Submitted!'))
+    .catch((err) => console.log(err))
+}
+
+// put edit request
+const editWord = (wordObj, callback) => {
+  axios.put('/words', wordObj)
+    .then(() => console.log('Definition Updated!'))
+    .catch((err) => console.log(err))
+}
+
+// delete request
+const deleteWord = (word, callback) => {
+  axios.delete('/words', { params: { word } })
+    .then(() => console.log('Word Deleted!'))
+    .catch((err) => console.log(err))
+}
