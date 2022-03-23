b2b939e2b49b4cba792b02680592f824aa929381
Matthew Pak <mattpak@gmail.com>
2022-03-23 10:41:49 -0700

    Added server app.get and app.post fx (untested)

--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -5,12 +5,32 @@ const {addWord, getWords} = require('./db.js')
 
 const app = express();
 
+app.use(json());
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // all words fetch
 app.get('/words', (req, res) => {
+  getWords((err, results) => {
+    if (err) {
+      res.sendStatus(404)
+    } else {
+      res.status(200).send(results)
+    }
+  })
+})
 
+// new word post
+app.post('/words', (req, res) => {
+  let newWord = req.body.word;
+  let newDef = req.body.definition;
+  addWord(newWord, newDef, (err) => {
+    if (err) {
+      res.sendStatus(400)
+    } else (
+      res.sendStatus(201)
+    )
+  })
 })
 
 app.listen(process.env.PORT);
