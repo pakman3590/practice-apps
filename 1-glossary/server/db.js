ab4b4a8880eefee3aff846e7fa2ebdb66c5f993a
Matthew Pak <mattpak@gmail.com>
2022-03-23 11:14:47 -0700

    Refactored db module.exports for cleaner exporting; modified wordSchema to use word as _id (TESTED)

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -15,33 +15,32 @@ mongoose.connect(process.env.MONGOURL || 'mongodb://localhost/glossary', {
 // 4. Import the models into any modules that need them
 
 const wordSchema = new Schema({
-  word: String,
+  _id: String,
   definition: String
 });
 
 const Word = mongoose.model('Word', wordSchema);
 
+module.exports = {
+  // add word fx
+  addWord(word, definition, callback) {
+    console.log('DB POST')
+    const newWord = new Word({ _id: word, definition });
+    newWord.save()
+      .then(() => callback(null))
+      .catch((err) => callback(err))
+  },
 
-// add word fx
-const addWord = (word, definition, callback) => {
-  console.log('DB POST')
-  const newWord = new Word({ word, definition });
-  newWord.save()
-    .then(() => callback(null))
-    .catch((err) => callback(err))
+  // get all words fx
+  getWords(callback) {
+    console.log('DB GET')
+    Word.find({}, (err, results) => {
+      if (err) {
+        callback(err)
+      } else {
+        callback(null, results)
+      }
+    })
+  },
+
 }
-
-// get all words fx
-const getWords = (callback) => {
-  console.log('DB GET')
-  Word.find({}, (err, results) => {
-    if (err) {
-      callback(err)
-    } else {
-      callback(null, results)
-    }
-  })
-}
-
-module.exports.addWord = addWord;
-module.exports.getWords = getWords;
